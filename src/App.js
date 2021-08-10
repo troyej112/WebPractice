e14bad2b119a4815bd9db1077e62b842fac903b4
troyej112 <58873501+troyej112@users.noreply.github.com>
2021-08-10 13:18:01 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>Sign up</h2>
+
+      <div className="ContainerLogin"></div>
+    </div>
+  );
+}
